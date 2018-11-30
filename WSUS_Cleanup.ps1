866b811daad0c267f31cfba8f6d388dedec73ce9
Janelle <jcahee@wswheboces.org>
2018-11-30 09:09:32 -0500

    Create WSUS_Cleanup.ps1

---
+++ WSUS_Cleanup.ps1
@@ -0,0 +1,7 @@
+﻿net stop wsusservice
+cd "C:\Program Files\Update Services\Tools"
+wsusutil.exe reset
+echo Delete WSUS Folder Content
+Remove-Item -path "C:\WSUS\WsusContent\*" -Recurse
+pause
+net start wsusservice
