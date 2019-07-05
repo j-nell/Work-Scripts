1f568fff8e8bca325cf07d0310b25969e67421a0
j-nell <39954560+j-nell@users.noreply.github.com>
2019-07-05 08:25:42 -0400

    Update C Drive Cleanup v1.ps1

--- C Drive Cleanup v1.ps1
+++ C Drive Cleanup v1.ps1
@@ -119,8 +119,8 @@ Format-Table -AutoSize | Out-String
 ## Sends some before and after info for ticketing purposes 
 
 Hostname ; Get-Date | Select-Object DateTime 
-Write-Verbose "Before:" $Before 
-Write-Verbose "After:" $After 
+Write-Verbose Before: $Before 
+Write-Verbose After: $After 
 Write-Verbose $size 
 ## Completed Successfully! 
 #Stop-Transcript 
